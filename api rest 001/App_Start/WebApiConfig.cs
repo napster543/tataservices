735a054c4becad42a0999eab15a6f23a61c15f4b
napster543 <napster543@gmail.com>
2021-11-23 10:32:39 -0500

    API
    Archivos de api completa

---
+++ api rest 001/App_Start/WebApiConfig.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using api_rest_001.Controllers;
+
+
+namespace api_rest_001
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+
+            // Web API configuration and services
+            //var cors = new EnableCorsAttribute("*", "*", "*", "*");
+            //config.EnableCors(cors);
+            // Web API routes
+
+            // Configuración y servicios de API web
+            
+            var corsAttr = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(corsAttr);            
+            // Rutas de API web
+            config.MapHttpAttributeRoutes();
+
+            config.MessageHandlers.Add(new TokenValidationHandler());
+            // HABILITAMOS EL SERIALIZADOR DE XML
+            //config.Formatters.XmlFormatter.UseXmlSerializer = true;
+            // ELIMINAMOS EL FORMATEADOR DE RESPUESTAS XML
+            config.Formatters.XmlFormatter.SupportedMediaTypes.Clear();
+            // HABILITAMOS EL FORMATEADOR DE RESPUESTAS JSON
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("application/json"));
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ api rest 001/Controllers/CustomersController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace api_rest_001.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api/customers")]
+    public class CustomersController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult GetId(int id)
+        {
+            var customerFake = "customer-fake";
+            return Ok(customerFake);
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetAll()
+        {
+            var customersFake = new string[] { "customer-1", "customer-2", "customer-3333" };
+            return Ok(customersFake);
+        }
+        [HttpPost]
+        
+        public IHttpActionResult repitepalabra(string palabra, string buscar)
+        {
+           string mensaje;
+
+            string frase = palabra; //Cadena
+            string[] misplits = frase.Split(new char[] { ' ' }); //Convierte la cadena en array
+            int CountItems1 = (from Itempalabra in misplits
+                                              where (Itempalabra.ToLower() == buscar)
+                                              select Itempalabra).ToList().Count;
+
+            var encontradas =  (from Itempalabra in misplits
+                               where (Itempalabra.ToLower() == buscar)
+                               select new { 
+                                   palabras = Itempalabra, 
+                                   contar = Itempalabra.Count()  } ).ToList();
+
+            
+
+            
+
+            if (CountItems1 > 0) 
+            {
+                string vercadena = "";
+                foreach (var x in encontradas)
+                {
+                    vercadena += x.palabras.ToString() + " - ";                
+                }
+                mensaje = vercadena + " encontradas " + encontradas.ToList().Count().ToString();
+            }
+            else
+            {
+                mensaje = "No se encontro palabras";
+            }
+           
+            
+            return Ok(mensaje);
+        }
+
+        
+    }
+}
